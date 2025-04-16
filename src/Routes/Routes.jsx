39ba7bd6329cde51a6e803bd68819a99a20d98db
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-17 02:23:24 +0600

    solve header section. something is baki

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     createBrowserRouter,
 } from "react-router";
 import Root from '../Pages/Root/Root';
 import ErrorPage from '../Pages/Error/ErrorPage';
+import Home from '../Pages/Home/Home';
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: Root,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                path: '/',
+                Component: Home
+            }
+        ]
     },
 ]);
 
