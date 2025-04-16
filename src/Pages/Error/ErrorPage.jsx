55552f90a3f7879ab5a31bf1182cd9ca1a796b13
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-17 02:07:28 +0600

    added home files

--- src/Pages/Error/ErrorPage.jsx
+++ src/Pages/Error/ErrorPage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ErrorPage = () => {
     return (
-        <div>
-
+        <div className='flex justify-center items-center min-h-screen'>
+            <h1 className='text-3xl '>Page Not Found</h1>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import {
     createBrowserRouter,
 } from "react-router";
 import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/Error/ErrorPage';
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: Root,
+        errorElement: <ErrorPage />
     },
 ]);
 
