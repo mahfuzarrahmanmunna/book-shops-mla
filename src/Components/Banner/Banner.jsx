943667271102b91d9c10731013c968a509fa8620
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-17 03:09:14 +0600

    added books files in json data

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='flex justify-around  items-center bg-[#f3f3f3]  rounded-3xl my-16 py-10'>
+        <div className='lg:flex justify-around  items-center bg-[#f3f3f3]  rounded-3xl my-16 py-10'>
             <div>
                 <h1 className='text-5xl font-bold'>Books to freshen up <br /> your bookshelf</h1>
                 <button className='btn btn-success mt-4'>
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Books = () => {
+    const [allBooks, setAllBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('/booksData.json')
+            .then(res => res.json())
+            .then(data => setAllBooks(data))
+    }, [])
     return (
         <div>
-            
+            I'm Books
         </div>
     );
 };
