7ea1f939f5296ebb6bf8f3651fe6bbc52a0e37d0
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-17 12:09:49 +0600

    added coard in books

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, } from 'react';
+import Book from '../Book/Book';
 
-const Books = () => {
-    const [allBooks, setAllBooks] = useState([]);
+const Books = ({ books }) => {
 
-    useEffect(() => {
-        fetch('/booksData.json')
-            .then(res => res.json())
-            .then(data => setAllBooks(data))
-    }, [])
+
+    // useEffect(() => {
+    //     fetch('/booksData.json')
+    //         .then(res => res.json())
+    //         .then(data => setAllBooks(data))
+    // }, [])
+
     return (
         <div>
             <h1 className='text-3xl text-center'>I'm Books</h1>
+            <Suspense fallback={<span>Loading ....</span>}>
+                <div className='grid grid-cols-3 gap-8 mb-8'>
+                    {
+                        books.map((book, index) => <Book book={book} key={index} />)
+                    }
+                </div>
+            </Suspense>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import Books from '../Books/Books';
+import { useLoaderData } from 'react-router';
 
 const Home = () => {
+    const books = useLoaderData()
     return (
         <div>
             <Banner />
-            <Books />
+            <Books books={books} />
         </div>
     );
 };
